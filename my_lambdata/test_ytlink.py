4654c8c62cb0ccee5e9a3db685640f162db11d9c
vjmiyagi <velmamiyagi@gmail.com>
2020-07-19 15:52:11 -0500

    Before running autopep8

--- my_lambdata/test_ytlink.py
+++ my_lambdata/test_ytlink.py
@@ -4,9 +4,9 @@ from my_lambdata.ds_utilities import ytlink
 print("Ready to create your link...")
 print()
 l = input("Please enter the link to the YouTube video: ")
-h = int(input("Please enter the hour in the timemark: "))
-m = int(input("Please enter the minutes in the timemark: "))
-s = int(input("Please enter the seconds in the timemark: "))
+h = int(input("Please enter the hour of the timemark: "))
+m = int(input("Please enter the minutes of the timemark: "))
+s = int(input("Please enter the seconds of the timemark: "))
 print()
 
 result = ytlink(l, h, m, s)
